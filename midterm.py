2e3d7765c53d0e24beb5fd0e2b64b3ed3fe0c49d
haseebtehsin18 <156181854+haseebtehsin18@users.noreply.github.com>
2024-01-17 21:50:54 +0500

    Add files via upload

---
+++ midterm.py
@@ -0,0 +1,12 @@
+# Abdul Haseeb Tehseen
+
+# Task 1
+#Write a Python program to do arithmetical operations addition and division.
+
+num = int(input("Your first num: 5")
+num = int(input("Your second num: 6")
+num1 = 5
+num2 = 6
+sum = int(num1 = num2)
+print("The sum of number is:",sum)
+ 
